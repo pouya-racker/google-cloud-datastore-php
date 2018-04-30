ff6a9ad810163ada6ab4457811cb10a686e9c147
Pouya Naghizadeh <pouya.naghizadeh@rackspace.com>
2018-04-29 21:00:25 -0700

    cleaning up loaddatastore.sh

--- loaddatastore.sh
+++ loaddatastore.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 FILE=$1
-COUNT=${2:-52000}
+COUNT=${2:-`wc -l $FILE|awk '{print $1}'`}
 
 readarray -t LINES < "$FILE"
 for LINE in -n $COUNT "${LINES[@]}"; do
+    echo "Reading line $LINE"
 	sku=`echo $LINE | sed "s/^.\?{\"sku\":\([0-9]\+\),\"name\":\"\([^\"]\+\)\".*$/\1/g"`
 	name=`echo $LINE | sed "s/^.\?{\"sku\":\([0-9]\+\),\"name\":\"\([^\"]\+\)\".*$/\2/g"`
+	echo "Inserting $sku , $name"
 	php loaddatastore.php "$sku" "$name"
 done
